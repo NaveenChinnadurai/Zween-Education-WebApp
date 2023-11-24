cfcc86b0f1e572233e2bc1f0832e8f924c56da11
Naveen Chinnadurai <naveenchinnadurai2019@gmail.com>
2023-11-24 21:58:48 +0530

    error:fixed at file name

--- src/components/homePageCompo/logosList.jsx
+++ src/components/homePageCompo/logosList.jsx
@@ -4,7 +4,7 @@ import img2 from '../../assets/Icons/acer.png'
 import img3 from '../../assets/Icons/motorola.png'
 import img4 from '../../assets/Icons/lenova.png'
 import img5 from '../../assets/Icons/microsoft.jpg'
-import img6 from '../../assets/Icons/sony.png'
+import img6 from '../../assets/Icons/Sony.png'
 import img7 from '../../assets/Icons/meta.png'
 
 function LogoDisplay() {
